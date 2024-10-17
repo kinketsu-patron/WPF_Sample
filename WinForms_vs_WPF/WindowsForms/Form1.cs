9888b75252aa6a9f36f2f737a28e065c6a1479b9
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-10-17 23:58:16 +0900

    Formsコード修正

--- WinForms_vs_WPF/WindowsForms/Form1.cs
+++ WinForms_vs_WPF/WindowsForms/Form1.cs
@@ -5,15 +5,15 @@ namespace WindowsForms
 {
     public partial class Form1 : Form
     {
-        private uint _Acount;
-        private uint _Bcount;
+        private uint m_Acount;
+        private uint m_Bcount;
 
         public Form1( )
         {
             InitializeComponent( );
 
-            _Acount = 0;
-            _Bcount = 0;
+            m_Acount = 0;
+            m_Bcount = 0;
 
             BtnA.Enabled = true;
             BtnB.Enabled = false;
@@ -21,16 +21,16 @@ namespace WindowsForms
 
         private void ButtonA_Click( object sender, EventArgs e )
         {
-            _Acount++;
-            Lbl_AClick_Count.Text = _Acount.ToString( );
+            m_Acount++;
+            Lbl_AClick_Count.Text = m_Acount.ToString( );
             BtnA.Enabled = false;
             BtnB.Enabled = true;
         }
 
         private void ButtonB_Click( object sender, EventArgs e )
         {
-            _Bcount++;
-            Lbl_BClick_Count.Text = _Bcount.ToString( );
+            m_Bcount++;
+            Lbl_BClick_Count.Text = m_Bcount.ToString( );
             BtnA.Enabled = true;
             BtnB.Enabled = false;
         }
